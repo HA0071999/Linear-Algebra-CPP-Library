66e30118b27662cd4cbd5bcb70752dc9b5f1fa79
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-24 13:45:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+class createMatrix{
+    int rows; int columns;
+    cout<<"Enter the number of rows of matrix: "<<endl;
+    cin>>rows;
+    cout<<"Enter the number of columns of matrix: "<<endl;
+    cin>>columns;
+    double matrix[rows][columns];
+};
+int main() {
+
+
+    return 0;
+}
